3b2d96906f516de82939581c58537076feb3074e
Gary Wang <git@blumia.net>
2023-10-14 22:48:34 +0800

    fix: QuickSetting Tile crash on Android 14
    Sidenote: MODE_BACKGROUND_ACTIVITY_START_ALLOWED for ActivityOptions
    might not be needed since the current fix works fine on my Android 14
    device...

--- app/src/main/java/net/blumia/pineapple/lockscreen/QuickSettingService.kt
+++ app/src/main/java/net/blumia/pineapple/lockscreen/QuickSettingService.kt
@@ -1,7 +1,9 @@
 package net.blumia.pineapple.lockscreen
 
+import android.app.PendingIntent
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.os.Build
 import android.service.quicksettings.TileService
 import net.blumia.pineapple.lockscreen.shortcuts.LockScreenShortcut
 
@@ -10,7 +12,12 @@ class QuickSettingService: TileService() {
     override fun onClick() {
         super.onClick()
         val intent = Intent(this, LockScreenShortcut::class.java)
-        intent.flags = FLAG_ACTIVITY_NEW_TASK
-        startActivityAndCollapse(intent)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            startActivityAndCollapse(PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE))
+        } else {
+            intent.flags = FLAG_ACTIVITY_NEW_TASK
+            @Suppress("DEPRECATION")
+            startActivityAndCollapse(intent)
+        }
     }
 }
